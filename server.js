42617c47ff8fcb02a90e829d4f5b35d942b2caf9
Amlan <amlansasmal19@gmail.com>
2023-08-21 20:18:11 +0530

    okk

--- server.js
+++ server.js
@@ -1,28 +1,43 @@
-const express= require('express');
-const ejs=require('ejs')
-const  expresslayout=require('express-ejs-layouts')
+const express = require('express');
+const ejs = require('ejs')
+const expresslayout = require('express-ejs-layouts')
 
-const path=require('path')
-const app= express();
-app.use(express.static('public'));//to run realtime on website
+const path = require('path')
 
-const PORT= process.env.PORT|| 4000
+const app = express();
 
-
+const PORT = process.env.PORT || 4000
 
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+// assets 
+
+app.use(express.static('public'))
+
+
+
 
 //set template engine
 
 app.use(expresslayout)
 
-app.set('views',path.join(__dirname,'/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'))
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.listen(PORT,()=>{
-    console.log(`listening on port  ${PORT} `); 
+
+app.get('/', (req, res) => {
+    res.render('home')
 })
+
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port  ${PORT} `);
+})
+
+
